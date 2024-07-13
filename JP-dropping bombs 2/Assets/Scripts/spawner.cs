5bbf56aa84e36fea42390431ab052f107a6c28df
JuggerNutJoe <130806226+MinnieMia@users.noreply.github.com>
2024-07-13 13:27:38 -0500

    womp

--- JP-dropping bombs 2/Assets/Scripts/spawner.cs
+++ JP-dropping bombs 2/Assets/Scripts/spawner.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public GameObject bombPrefab;
-    public float delay 2.0f;
+    public float delay;
     public bool active = true;
     public Vector2 delayRange = new Vector2(1,2); 
+    public Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         ResetDelay();
         StartCoroutine(EnemyGenerator());
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = bombPrefab.GetComponent<MeshRenderer>().bounds.size.x / 2;
+        objectHeight = bombPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
@@ -27,10 +34,18 @@ public class spawner : MonoBehaviour
         yield return new WaitForSeconds(delay);
         if (active)
         {
-            Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation)
+            float randomX = Random.Range(screenBounds.x - objectWidth, screenBounds.x * -1 + objectWidth);
+            float spawnY = (screenBounds.y + objectHeight) + 5;
+
+            Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
             ResetDelay();
         }
 
         StartCoroutine(EnemyGenerator());
+    }
+
+    void ResetDelay()
+    {
+        delay = Random.Range(delayRange.x, delayRange.y);
     }
 }
